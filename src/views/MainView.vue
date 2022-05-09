99140de58f63f67f65f9848dddb1150bc5000a29
oseunghyun <ohsh1477@gmail.com>
2022-05-09 09:56:35 +0900

    CROF-43 [feat] 제보하기 - 모달 컴포넌트 구현, 제보하기 페이지 구현

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,8 +5,11 @@
     <div class="cafe-view">
       <router-view></router-view>
       <cafe-menu-component></cafe-menu-component>
-      <button class="btn--primary">제보하기 +</button>
+      <button @click="isModalActive = true" class="btn--primary">제보하기 +</button>
       <button class="btn--border">리뷰 작성하기 +</button>
+      <modal-component v-if="isModalActive" >
+        <modal-content @close-modal="isModalActive = false"/>
+      </modal-component>
     </div>
   </div>
     
@@ -17,13 +20,16 @@
 import MainHeader from '@/components/MainHeader.vue';
 import SearchbarComponent from '../components/Main/SearchbarComponent.vue';
 import CafeMenuComponent from '../components/Cafe/CafeMenuComponent.vue';
+import ModalComponent from '../components/Modal/ModalComponent.vue';
+import ModalContent from '../components/Modal/ModalContent.vue';
 
 
 export default {
-  components: { MainHeader, SearchbarComponent, CafeMenuComponent },
+  components: { MainHeader, SearchbarComponent, CafeMenuComponent, ModalComponent, ModalContent },
   data() {
     return {
-      page: 'main'
+      page: 'main',
+      isModalActive: false,
     }
   },
 }
