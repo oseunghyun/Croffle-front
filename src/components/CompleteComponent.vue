52f4bfe73316ea43f78e3c3620a6bc3adb5087a5
oseunghyun <ohsh1477@gmail.com>
2022-05-12 23:17:34 +0900

    CROF-48 [feat] 사장님 페이지 UI 개발

---
+++ src/components/CompleteComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="complete-component">
+    <div class="complete__wrapper">
+      <img :src="ic__complete" />
+      <h3>{{ message }} 완료되었습니다.</h3>
+
+      <button @click="toHome" class="btn--primary">홈으로</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ic__complete from "@/assets/ic/complete.svg";
+export default {
+  data() {
+    return {
+      ic__complete,
+    };
+  },
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    toHome() {
+      this.$router.push("/owner");
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { router } from './routes/index';
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./routes/index";
 // import store from './store';
-import './assets/scss/_index.scss';
-
+import "./assets/scss/_index.scss";
 
 const app = createApp(App);
 
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
