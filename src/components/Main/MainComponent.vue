2ae8af4845410f22c1ee955c1b2a0ae0324e753e
oseunghyun <ohsh1477@gmail.com>
2022-05-16 12:44:16 +0900

    [add] 테스트용 말풍선 버튼 추가

--- src/components/Main/MainComponent.vue
+++ src/components/Main/MainComponent.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="main-container">
     <p class="guide">
-      원정대에게 알려주고 싶은 카페가 있나요?<br>
+      원정대에게 알려주고 싶은 카페가 있나요?<br />
       카페를 찾아 크로플 원정대에 제보해주세요!
     </p>
+    <button @click="toDetail" class="btn--transparent">
+      <img :src="ic__speechBubble" />
+    </button>
   </div>
 </template>
 
 <script>
-
+import ic__speechBubble from "@/assets/ic/speechBubble.svg";
 export default {
   data() {
     return {
-    }
+      ic__speechBubble,
+    };
   },
-}
+  methods: {
+    toDetail() {
+      let pageNum = 6;
+      this.$emit("toDetail", pageNum);
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
