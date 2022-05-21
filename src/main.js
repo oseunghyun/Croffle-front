b51f96c3c2e1fa3e3f50f40ea83f1c0868d65082
oseunghyun <ohsh1477@gmail.com>
2022-05-21 11:24:54 +0900

    [chore] CROF-55 스토어 세팅

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./routes/index";
-// import store from './store';
+import { store } from "./store/index";
 import "./assets/scss/_index.scss";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
 app.mount("#app");
