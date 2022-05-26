3854e50fcdcc4e3b65eb9e3099ca70805b66db35
oseunghyun <ohsh1477@gmail.com>
2022-05-26 16:16:53 +0900

    [del] 랜딩컴포넌트, 로그인 뷰, getByte(), 삭제 및 라우터 수정

--- src/utils/validation.js
+++
@@ -1,9 +0,0 @@
-// 바이트 계산기
-const getByte = (str) => {
-  return str
-    .split("")
-    .map((s) => s.charCodeAt(0))
-    .reduce((prev, c) => prev + (c === 10 ? 2 : c >> 7 ? 2 : 1), 0);
-};
-
-export { getByte };
