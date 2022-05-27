b0fc89f8778e64edd69e85c2194a4cf4b82fdb2a
oseunghyun <ohsh1477@gmail.com>
2022-05-27 22:50:47 +0900

    [refactor] 엑시오스 인터셉터 모듈화

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,14 @@
 import axios from "axios";
+import { setInterceptors } from "./common/interceptors";
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+// 액시오스 초기화 함수
+function createInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+  return setInterceptors(instance);
+}
+const instance = createInstance();
 
 // 리뷰 작성하기
 function registerReview(reviewData) {
@@ -24,4 +30,33 @@ function fetchCafeInfo() {
   return instance.get("cafe");
 }
 
-export { registerReview, reportMenu, reportInfo, fetchCafeInfo };
+// // 카페 추천 기능
+// function recommendCafe() {
+//   const params = { filter: ["liked", "review"] };
+//   return instance.get("cafe/recommend", { params });
+// }
+
+// 커뮤니티 게시글 작성
+function createPost(postData) {
+  return instance.post("board", postData);
+}
+
+// 커뮤니티 게시글 조회
+function fetchPosts() {
+  return instance.get("board");
+}
+
+// // 로그인
+// function loginUser() {
+//   return instance.get("oauth2/authorization/naver");
+// }
+
+export {
+  registerReview,
+  reportMenu,
+  reportInfo,
+  fetchCafeInfo,
+  createPost,
+  fetchPosts,
+  // loginUser,
+};
