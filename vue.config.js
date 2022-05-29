45bd8c20c446dd12c08680653f290847f9068fa4
oseunghyun <ohsh1477@gmail.com>
2022-05-30 01:35:46 +0900

    [feat] CROF-108 네이버 카페 검색 기능 구현, 카페 제보 api 함수 구현

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
-		loaderOptions: {
-			scss: {
-        additionalData: `@import "@/assets/scss/variables.scss";`
-			},
-		},
-	},
-})
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/variables.scss";`,
+      },
+    },
+  },
+  devServer: {
+    proxy: {
+      "/v1": {
+        target: "https://openapi.naver.com/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
