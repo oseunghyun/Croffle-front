3de5044265b8d3e9f0437736d85c510d603d516e
oseunghyun <ohsh1477@gmail.com>
2022-06-03 22:52:07 +0900

    [fix] CROF-57 카페 추천 게시판 api 함수 params 수정

--- src/api/cafe.js
+++ src/api/cafe.js
@@ -1,15 +1,15 @@
-/* 카페 관련 서비스 api 함수 */
-import { cafe } from "@/api/cafe";
+// /* 카페 관련 서비스 api 함수 */
+// import { cafe } from "@/api/index";
 
-// 카페 상세정보 조회
-function fetchCafeInfo(cafeId) {
-  return cafe.get(`id/${cafeId}`);
-}
+// // 카페 상세정보 조회
+// function fetchCafeInfo(cafeId) {
+//   return cafe.get(`id/${cafeId}`);
+// }
 
-// 카페 추천 기능
-function recommendCafe() {
-  const params = { filter: ["liked", "review"] };
-  return cafe.get("recommend", { params });
-}
+// // 카페 추천 기능
+// function recommendCafe() {
+//   const params = { filter: ["liked", "review"] };
+//   return cafe.get("recommend", { params });
+// }
 
-export { fetchCafeInfo, recommendCafe };
+// export { fetchCafeInfo, recommendCafe };
