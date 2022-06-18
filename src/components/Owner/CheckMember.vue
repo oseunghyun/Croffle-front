64b67afa806285afa7a9fc02b44ce58ac124069b
oseunghyun <ohsh1477@gmail.com>
2022-06-18 18:55:06 +0900

    [add] CROF-128 회원 조회 시 전화번호 하이픈 변환 추가

--- src/components/Owner/CheckMember.vue
+++ src/components/Owner/CheckMember.vue
@@ -8,12 +8,13 @@
       <div class="input__box">
         <label>회원 전화번호</label>
         <input
-          v-model="user_phone"
+          v-model="contact"
           placeholder="회원 전화번호를 입력해주세요."
-          maxlength="15"
+          maxlength="20"
           oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1')"
+          @keyup="getPhoneMask(contact)"
         />
-        <span class="count">{{ phoneLength }}/15</span>
+        <span class="count">{{ phoneLength }}/20</span>
       </div>
     </form>
     <button
@@ -35,7 +36,7 @@ export default {
     return {
       cafe_id: "",
       user_id: "",
-      user_phone: "",
+      contact: "",
       stampInfo: {
         user_nickname: "",
         stamp_cnt: 0,
@@ -44,17 +45,81 @@ export default {
   },
   computed: {
     isValid() {
-      if (this.user_phone) {
+      if (this.contact) {
         return true;
       } else {
         return false;
       }
     },
     phoneLength() {
-      return this.user_phone.length;
+      return this.contact.length;
     },
   },
   methods: {
+    getPhoneMask(val) {
+      let res = this.getMask(val);
+      this.contact = res;
+    },
+    getMask(phoneNumber) {
+      if (!phoneNumber) return phoneNumber;
+      phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
+      let res = "";
+      if (phoneNumber.length < 3) {
+        res = phoneNumber;
+      } else {
+        if (phoneNumber.substr(0, 2) == "02") {
+          if (phoneNumber.length <= 5) {
+            //02-123-5678
+            res = phoneNumber.substr(0, 2) + "-" + phoneNumber.substr(2, 3);
+          } else if (phoneNumber.length > 5 && phoneNumber.length <= 9) {
+            //02-123-5678
+            res =
+              phoneNumber.substr(0, 2) +
+              "-" +
+              phoneNumber.substr(2, 3) +
+              "-" +
+              phoneNumber.substr(5);
+          } else if (phoneNumber.length > 9) {
+            //02-1234-5678
+            res =
+              phoneNumber.substr(0, 2) +
+              "-" +
+              phoneNumber.substr(2, 4) +
+              "-" +
+              phoneNumber.substr(6);
+          }
+        } else {
+          if (phoneNumber.length < 8) {
+            res = phoneNumber;
+          } else if (phoneNumber.length == 8) {
+            res = phoneNumber.substr(0, 4) + "-" + phoneNumber.substr(4);
+          } else if (phoneNumber.length == 9) {
+            res =
+              phoneNumber.substr(0, 3) +
+              "-" +
+              phoneNumber.substr(3, 3) +
+              "-" +
+              phoneNumber.substr(6);
+          } else if (phoneNumber.length == 10) {
+            res =
+              phoneNumber.substr(0, 3) +
+              "-" +
+              phoneNumber.substr(3, 3) +
+              "-" +
+              phoneNumber.substr(6);
+          } else if (phoneNumber.length > 10) {
+            //010-1234-5678
+            res =
+              phoneNumber.substr(0, 3) +
+              "-" +
+              phoneNumber.substr(3, 4) +
+              "-" +
+              phoneNumber.substr(7);
+          }
+        }
+      }
+      return res;
+    },
     async fetchMembership() {
       try {
         console.log("회원정보 조회");
